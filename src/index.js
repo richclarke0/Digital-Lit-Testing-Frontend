924ab8ad09ff2842c8c527a3de4883e9a7fba15b
Richard Clarke <freshnewrich@gmail.com>
2022-05-30 11:37:04 -0700

    different mdb install?

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 //import mdb 
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
