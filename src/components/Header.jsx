2c5a5374beb6972d61ade23fcd5cc5a7fcbd515a
Richard Clarke <freshnewrich@gmail.com>
2022-05-30 12:33:05 -0700

    footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Component(props) {
     return (
-        <div className="p-4 text-center bg-dark">
+        <div className="p-4 text-center bg-dark text-white">
             <i>
                 <h4>Digital Lit Questions</h4>
             </i>
--- src/components/NavFooter.jsx
+++ src/components/NavFooter.jsx
@@ -1,5 +1,14 @@
+import { MDBFooter, MDBIcon } from "mdb-react-ui-kit"
+
 export default function NavFooter(props) {
     return (
-        "foot"
+        <MDBFooter className="bg-dark p-2">
+            {/* //main page index  */}
+            <a className='btn btn-light btn-floating m-1' href='#!' role='button'>
+                <MDBIcon fas icon="list-ul" />
+            </a>
+            <div className="text-white">Main</div>
+            
+        </MDBFooter>
     )
 }
